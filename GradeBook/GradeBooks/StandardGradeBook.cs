ec403244b67ad720b1c536553be7ab2203d6ac1d
Kensuke Matsumoto <kensuke.matsumoto@woven-planet.global>
2021-10-19 12:56:48 +0900

    sln changed

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public string name;
+        public StandardGradeBook()
+        {
+            name = :base(name) ;
+
+
+        }
+    }
+}
