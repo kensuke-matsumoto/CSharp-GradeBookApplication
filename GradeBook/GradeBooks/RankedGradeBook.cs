d954ad2f4e48002a05b1355bf1bc018a730ae332
Kensuke Matsumoto <kensuke.matsumoto@woven-planet.global>
2021-10-19 15:53:43 +0900

    completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,7 +16,6 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Ranked grading requires at least 5 students.");
 
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
-            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageHrade).ToList();
 
             if (grades[threshold - 1] <= averageGrade)
                 return 'A';
